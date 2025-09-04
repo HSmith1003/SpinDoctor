a997bb3e354808aa2b8b453e50231f07cbb77721
Heston L Smith <39459377+HSmith1003@users.noreply.github.com>
2025-09-04 14:01:00 -0700

    Update SpinDoctor.py
    Added keyboard interrupt protocol for a killswitch

--- SpinDoctor.py
+++ SpinDoctor.py
@@ -40,147 +40,159 @@ stain_time = 90
 #Wash Volume, mL
 wash_vol = config['wash_vol']
 
-#Connect to the syringe pump
-logging.info('Connecting to the Multichannel Syringe Pump...', extra ={'weblog':True})
+#Initial Startup Message to User
+print("Starting SpinDoctor. Press CTRL+C to exit at any time")
+
 try:
-    pump = SY01B(S_COM_PORT, baudrate=9600, position_count=6, syringe_volume_ul=5000)
-    logging.info('Connection Successful!', extra ={'weblog':True})
-    #report on the status of the pump once connected
-    print(f"Address: {pump.get_address()}")
-    print(f"Baud rate: {pump.get_rs232_baudrate()}")
-    print(f"Firmware Version: {pump.get_firmware_version()}")
-    #initialize the pump to start
-    logging.info("Initializing Syringe...", extra={'weblog':True})
-    pump.move_valve_to_position(WASTE)
-    pump.reset_syringe_position()
-    logging.info('Syringe Pump is Ready', extra ={'weblog':True})
-except Exception as e:
-    logging.error("Could not connect to the Multichannel Syringe Pump: {}" .format(e), extra ={"weblog":True})
-    exit()
-
-#Set up the subprocess to run ticcmd 
-def ticcmd(*args):
-    return subprocess.check_output(['ticcmd'] + list(args))
-#connect to the tic board using subprocess and get information on it's status
-logging.info('Connecting to the Stepper Motor...', extra ={'weblog':True})
-try:
-    status = yaml.safe_load(ticcmd('-s', '--full'))
-    ticname = status['Name']
-    serialnum = status ['Serial number']
-    firmware = status['Firmware version']
-    vin = status['VIN voltage']
-    energize = status['Energized']
-    logging.info('Connection Successful!', extra ={'weblog':True})
-    print("Tic Board Name: {}".format(ticname))
-    print("Serial Number: {}".format(serialnum))
-    print("Firmware Version: {}".format(firmware))
-    print("VIN Voltage: {}" .format(vin))
-    print("Energized?: {}".format(energize))
-    #Test the motor by energizing it and moving it forward and backward
-    logging.info("Testing Motor...", extra ={'weblog':True})
-    ticcmd('--energize')
-    ticcmd('--exit-safe-start')                                                    
-    ticcmd('--velocity', str(400000) )
-    sleep(3)
-    ticcmd('--halt-and-hold')
-    ticcmd('--velocity', str(-400000))
-    sleep(3)
-    ticcmd('--halt-and-hold')
-    ticcmd('--deenergize')
-    logging.info("Motor is ready", extra ={'weblog':True})
-except Exception as e:
-    logging.error("Could not connect to the Stepper Motor Controller - {}".format(e), extra ={"weblog":True})
-    exit()
-
-#Purge the lines initially
-print("Purging fill line with air...")
-pump.move_valve_to_position(AIR)
-pump.withdraw(5000)
-pump.move_valve_to_position(CHAMBER)
-pump.reset_syringe_position()
-
-#Drain the chamber initially
-print ("Draining any residuals in the Chamber")
-for i in range(2):
-        pump.move_valve_to_position(DRAIN)
-        pump.withdraw(5000)#Prompt the user to set up for washing and press enter to continue
+    #Connect to the syringe pump
+    logging.info('Connecting to the Multichannel Syringe Pump...', extra ={'weblog':True})
+    try:
+        pump = SY01B(S_COM_PORT, baudrate=9600, position_count=6, syringe_volume_ul=5000)
+        logging.info('Connection Successful!', extra ={'weblog':True})
+        #report on the status of the pump once connected
+        print(f"Address: {pump.get_address()}")
+        print(f"Baud rate: {pump.get_rs232_baudrate()}")
+        print(f"Firmware Version: {pump.get_firmware_version()}")
+        #initialize the pump to start
+        logging.info("Initializing Syringe...", extra={'weblog':True})
         pump.move_valve_to_position(WASTE)
-        pump.dispense(5000)
-        
-input ("Place the samples in the wash tray and place the wash tray in the basket. Press ENTER to initiate WASH Cycle. Press K to kill the script at any time.")
-
+        pump.reset_syringe_position()
+        logging.info('Syringe Pump is Ready', extra ={'weblog':True})
+    except Exception as e:
+        logging.error("Could not connect to the Multichannel Syringe Pump: {}" .format(e), extra ={"weblog":True})
+        exit()
     
-# ~~~~~~~~~~ WASH CYCLE ~~~~~~~~~~
-for i in range(num_washes):
-    #prime the syringe to draw water
-    print ("Priming Wash Liquid")
-    pump.move_valve_to_position(FLUID_1)
-    pump.withdraw(1500)
-    sleep(0.5)
-    pump.dispense(1500)
-    sleep(0.5)
-    #Draw water and fill the tank
-    print("Filliing the Chamber...")
-    num_fills = int((wash_vol)/5)
-    for j in range(num_fills):
-        pump.withdraw(5000)
-        pump.move_valve_to_position(CHAMBER)
-        pump.dispense(5000)
-        pump.move_valve_to_position(FLUID_1)
-    #Purge the lines again with air to make sure all liquid is in the chamber 
-    print ("Purging fill line to finish fill...")
+    #Set up the subprocess to run ticcmd 
+    def ticcmd(*args):
+        return subprocess.check_output(['ticcmd'] + list(args))
+    #connect to the tic board using subprocess and get information on it's status
+    logging.info('Connecting to the Stepper Motor...', extra ={'weblog':True})
+    try:
+        status = yaml.safe_load(ticcmd('-s', '--full'))
+        ticname = status['Name']
+        serialnum = status ['Serial number']
+        firmware = status['Firmware version']
+        vin = status['VIN voltage']
+        energize = status['Energized']
+        logging.info('Connection Successful!', extra ={'weblog':True})
+        print("Tic Board Name: {}".format(ticname))
+        print("Serial Number: {}".format(serialnum))
+        print("Firmware Version: {}".format(firmware))
+        print("VIN Voltage: {}" .format(vin))
+        print("Energized?: {}".format(energize))
+        #Test the motor by energizing it and moving it forward and backward
+        logging.info("Testing Motor...", extra ={'weblog':True})
+        ticcmd('--energize')
+        ticcmd('--exit-safe-start')                                                    
+        ticcmd('--velocity', str(400000) )
+        sleep(3)
+        ticcmd('--halt-and-hold')
+        ticcmd('--velocity', str(-400000))
+        sleep(3)
+        ticcmd('--halt-and-hold')
+        ticcmd('--deenergize')
+        logging.info("Motor is ready", extra ={'weblog':True})
+    except Exception as e:
+        logging.error("Could not connect to the Stepper Motor Controller - {}".format(e), extra ={"weblog":True})
+        exit()
+    
+    #Purge the lines initially
+    print("Purging fill line with air...")
     pump.move_valve_to_position(AIR)
-    pump.withdraw(4500)
+    pump.withdraw(5000)
     pump.move_valve_to_position(CHAMBER)
     pump.reset_syringe_position()
-    sleep(0.5)
     
-    #Start the wash - prompt the user to continue using default wash time or to override with their own
-    wash_time = input ("Chamber has been filled. Please enter the desired wash time in minutes")
-    #Wash cycle
-    ticcmd('--energize')
-    ticcmd('--exit-safe-start')                                                    
-    wash_vel = 300000  #speed at which the motor spins durng the wash process (microsteps per 1000s)
-    ticcmd('--velocity', str(wash_vel) )
-    logging.info("Starting Wash {}/{}, time = {} minutes" .format( i+1, num_washes, wash_time), extra={'weblog':True})
-    #Run the wash for the alloted time and then stop the motor
-    wash_seconds = int(wash_time * 60)
-    for k in range(wash_seconds):
-        print("Washing |", end="\r", flush=True)
-        sleep(0.167)
-        print("Washing /",end="\r", flush=True)
-        sleep(0.167)
-        print("Washing \\",end="\r", flush=True)
-        sleep(0.167)
-        print("Washing |",end="\r", flush=True)
-        sleep(0.167)
-        print("Washing /",end="\r", flush=True)
-        sleep(0.167)
-        print("Washing \\",end="\r", flush=True)
-        sleep(0.167)
-    ticcmd('--velocity', str(0) )
-    #ledpin.write(0) #turn off the 'wash active' LED
-    logging.info("Wash {}/{} Complete!" .format(i+1,num_washes), extra={'weblog':True})
+    #Drain the chamber initially
+    print ("Draining any residuals in the Chamber")
+    for i in range(2):
+            pump.move_valve_to_position(DRAIN)
+            pump.withdraw(5000)#Prompt the user to set up for washing and press enter to continue
+            pump.move_valve_to_position(WASTE)
+            pump.dispense(5000)
+            
+    input ("Place the samples in the wash tray and place the wash tray in the basket. Press ENTER to initiate WASH Cycle. Press K to kill the script at any time.")
+    
+        
+    # ~~~~~~~~~~ WASH CYCLE ~~~~~~~~~~
+    for i in range(num_washes):
+        #prime the syringe to draw water
+        print ("Priming Wash Liquid")
+        pump.move_valve_to_position(FLUID_1)
+        pump.withdraw(1500)
+        sleep(0.5)
+        pump.dispense(1500)
+        sleep(0.5)
+        #Draw water and fill the tank
+        print("Filliing the Chamber...")
+        num_fills = int((wash_vol)/5)
+        for j in range(num_fills):
+            pump.withdraw(5000)
+            pump.move_valve_to_position(CHAMBER)
+            pump.dispense(5000)
+            pump.move_valve_to_position(FLUID_1)
+        #Purge the lines again with air to make sure all liquid is in the chamber 
+        print ("Purging fill line to finish fill...")
+        pump.move_valve_to_position(AIR)
+        pump.withdraw(4500)
+        pump.move_valve_to_position(CHAMBER)
+        pump.reset_syringe_position()
+        sleep(0.5)
+        
+        #Start the wash - prompt the user to continue using default wash time or to override with their own
+        wash_time = input ("Chamber has been filled. Please enter the desired wash time in minutes")
+        #Wash cycle
+        ticcmd('--energize')
+        ticcmd('--exit-safe-start')                                                    
+        wash_vel = 300000  #speed at which the motor spins durng the wash process (microsteps per 1000s)
+        ticcmd('--velocity', str(wash_vel) )
+        logging.info("Starting Wash {}/{}, time = {} minutes" .format( i+1, num_washes, wash_time), extra={'weblog':True})
+        #Run the wash for the alloted time and then stop the motor
+        wash_seconds = int(wash_time * 60)
+        for k in range(wash_seconds):
+            print("Washing |", end="\r", flush=True)
+            sleep(0.167)
+            print("Washing /",end="\r", flush=True)
+            sleep(0.167)
+            print("Washing \\",end="\r", flush=True)
+            sleep(0.167)
+            print("Washing |",end="\r", flush=True)
+            sleep(0.167)
+            print("Washing /",end="\r", flush=True)
+            sleep(0.167)
+            print("Washing \\",end="\r", flush=True)
+            sleep(0.167)
+        ticcmd('--velocity', str(0) )
+        #ledpin.write(0) #turn off the 'wash active' LED
+        logging.info("Wash {}/{} Complete!" .format(i+1,num_washes), extra={'weblog':True})
+        ticcmd('--deenergize')
+        
+        if i == (num_washes-1): 
+            #Prompt the user to remove the wash tray 
+            input ("Final Wash Complete. Remove sample tray and press ENTER to drain the chamber...")
+    
+        # ~~~~~~~~ DRAIN ~~~~~~~~
+        logging.info("Draining the Chamber...",extra={'weblog':True})
+        num_drains = int(num_fills + 2)
+        sleep(1)
+        for m in range(num_drains):
+            pump.move_valve_to_position(DRAIN)
+            pump.withdraw(5000)
+            pump.move_valve_to_position(WASTE)
+            pump.dispense(5000)
+        logging.info("Drain Complete!",extra={'weblog':True})
+    
+    #end of script - print a message saying goodbye
+    logging.info("Washes complete. Exiting program...", extra={'weblog':True})
+
+except KeyboardInterrupt: 
+    print("User Interrupt Recieved. Exiting...")
     ticcmd('--deenergize')
-    
-    if i == (num_washes-1): 
-        #Prompt the user to remove the wash tray 
-        input ("Final Wash Complete. Remove sample tray and press ENTER to drain the chamber...")
-
-    # ~~~~~~~~ DRAIN ~~~~~~~~
-    logging.info("Draining the Chamber...",extra={'weblog':True})
-    num_drains = int(num_fills + 2)
-    sleep(1)
-    for m in range(num_drains):
-        pump.move_valve_to_position(DRAIN)
-        pump.withdraw(5000)
-        pump.move_valve_to_position(WASTE)
-        pump.dispense(5000)
-    logging.info("Drain Complete!",extra={'weblog':True})
-
-#end of script - print a message saying goodbye
-logging.info("Washes complete. Exiting program...", extra={'weblog':True})
-
+    for n in range(4):
+            pump.move_valve_to_position(DRAIN)
+            pump.withdraw(5000)
+            pump.move_valve_to_position(WASTE)
+            pump.dispense(5000)
 
 
 
